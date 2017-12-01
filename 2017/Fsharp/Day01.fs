75475d847c3ca9ffbb194ae63b8c06465bc0d71e
Anders Feldt <anders@andersfeldt.se>
2017-12-01 10:34:30 +0100

    Day 1 A

--- 2017/Fsharp/Day01.fs
+++ 2017/Fsharp/Day01.fs
@@ -1,6 +1,20 @@
 module Day01
 
+let getResultA (s:string) =
+    let numbers =
+        s.ToCharArray()
+        |> Array.toSeq
+        |> Seq.map (fun c -> (c |> int) - ('0' |> int))
+
+    (Seq.last numbers) :: (Seq.toList numbers)
+    |> List.windowed 2
+    |> List.map (fun x -> x.[0], x.[1])
+    |> List.filter (fun (x, y) -> x = y)
+    |> List.sumBy fst
+    |> string
+
+
 let getResult part (input:string[]) =
     match part with
-    | A -> "foo"
+    | A -> getResultA input.[0]
     | B -> "bar"
