cfc89ec4ccea9d55537e9e9b3d3e5c31835fbb36
Anders Feldt <anders@andersfeldt.se>
2017-12-01 11:24:08 +0100

    Day 1 B

--- 2017/Fsharp/Day01.fs
+++ 2017/Fsharp/Day01.fs
@@ -1,5 +1,7 @@
 module Day01
 
+let isEqualPair (x, y) = x = y
+
 let getResultA (s:string) =
     let numbers =
         s.ToCharArray()
@@ -9,12 +11,27 @@ let getResultA (s:string) =
     (Seq.last numbers) :: (Seq.toList numbers)
     |> List.windowed 2
     |> List.map (fun x -> x.[0], x.[1])
-    |> List.filter (fun (x, y) -> x = y)
+    |> List.filter isEqualPair
     |> List.sumBy fst
     |> string
 
+let getResultB (s:string) =
+    let numbers =
+        s.ToCharArray()
+        |> Array.toList
+        |> List.map (fun c -> (c |> int) - ('0' |> int))
+
+    let halfLength = numbers.Length / 2
+    let firstHalf = numbers |> List.take halfLength
+    let secondHalf = numbers |> List.skip halfLength
+
+    List.zip firstHalf secondHalf
+    |> List.filter isEqualPair
+    |> List.sumBy fst
+    |> (fun x -> x * 2)
+    |> string
 
 let getResult part (input:string[]) =
     match part with
     | A -> getResultA input.[0]
-    | B -> "bar"
+    | B -> getResultB input.[0]
