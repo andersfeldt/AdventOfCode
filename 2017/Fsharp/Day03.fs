97191140f94e2301259147bdfbf4a82ea104404d
Anders Feldt <anders@andersfeldt.se>
2017-12-04 23:15:25 +0100

    Day 3 B

--- 2017/Fsharp/Day03.fs
+++ 2017/Fsharp/Day03.fs
@@ -26,7 +26,66 @@ let getResultA number =
     shortestDistanceToMidPoint + stepSize
     |> string
 
+let moveRight (x, y) = x + 1, y
+let moveLeft  (x, y) = x - 1, y
+let moveUp    (x, y) = x,     y - 1
+let moveDown  (x, y) = x,     y + 1
+
+let instructions =
+    let directions =
+        Seq.initInfinite (fun i ->
+            match i % 4 with
+            | 0 -> moveRight
+            | 1 -> moveUp
+            | 2 -> moveLeft
+            | _ -> moveDown)
+
+    let numberOfStepsPerDirection =
+        Seq.initInfinite (fun i -> (i + 2) / 2)
+
+    let repeatDirection (direction, numberOfSteps) =
+        [for _ in [1..numberOfSteps] do yield direction]
+
+    Seq.zip directions numberOfStepsPerDirection
+    |> Seq.collect repeatDirection
+
+let getSurroundingSum dictionary (x, y) =
+    let surroundingPositions =
+        [for x' in [-1..1] do
+            for y' in [-1..1] do
+                if x' <> 0 || y' <> 0 then
+                    yield (x + x', y + y')]
+
+    let getValueInPosition position =
+        match Map.tryFind position dictionary with
+        | Some value -> value
+        | None       -> 0
+
+    surroundingPositions
+    |> List.sumBy getValueInPosition
+
+let initialPosition = (0, 0)
+let initialValue = 1
+let initialDictionary = [(initialPosition, initialValue)] |> Map.ofList
+let initialState = (initialDictionary, initialPosition, initialValue)
+
+let scanFunc (dictionary, position, _) direction =
+    let newPosition = direction position
+    let surroundingSum = getSurroundingSum dictionary newPosition
+    let newDictionary = Map.add newPosition surroundingSum dictionary
+    (newDictionary, newPosition, surroundingSum)
+
+let stoppingCondition number (_, _, currentValue) =
+    currentValue > number
+
+let getResultB number =
+    instructions
+    |> Seq.scan scanFunc initialState
+    |> Seq.find (stoppingCondition number)
+    |> (fun (_, _, value) -> value)
+    |> string
+
 let getResult part (input:string[]) =
     match part with
     | A -> getResultA (input.[0] |> int)
-    | B -> failwith "Not implemented yet"
+    | B -> getResultB (input.[0] |> int)
