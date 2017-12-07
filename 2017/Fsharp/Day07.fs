bb4cae81a69262b2cab1e3d98515537ce93baea8
Anders Feldt <anders@andersfeldt.se>
2017-12-07 10:56:54 +0100

    Day 7 A

--- 2017/Fsharp/Day07.fs
+++ 2017/Fsharp/Day07.fs
@@ -1,7 +1,31 @@
 module Day07
 
+open System
+
+let getResultA (input:string list) =
+    let lineToPair (line:string) =
+        match (line.Split("(->), 0123456789".ToCharArray(), StringSplitOptions.RemoveEmptyEntries) |> List.ofArray) with
+        | [x]   -> (x, List.empty<string>)
+        | x::xs -> (x, xs)
+        | []    -> failwith "should not happen..."
+    let listOfPairs =
+        input
+        |> List.map lineToPair
+    let allNames =
+        listOfPairs
+        |> List.map fst
+        |> Set.ofList
+    let leavesAndSubTrees =
+        listOfPairs
+        |> List.collect snd
+        |> List.distinct
+        |> Set.ofList
+
+    Set.difference allNames leavesAndSubTrees
+    |> Set.toSeq
+    |> Seq.exactlyOne
+
 let getResult part (input:string list) =
     match part with
-    | A -> failwith "Not implemented yet"
+    | A -> getResultA input
     | B -> failwith "Not implemented yet"
-    |> string
