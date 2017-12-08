b51079ed85892cbf8c804aa829a7ab6360aaa04f
Anders Feldt <anders@andersfeldt.se>
2017-12-08 23:41:30 +0100

    Day 8 B

--- 2017/Fsharp/Day08.fs
+++ 2017/Fsharp/Day08.fs
@@ -11,39 +11,48 @@ type Instruction =
         compareValue:int;
     }
 
+let getInstructions input =
+    let parseLine line =
+        let getPredicate token =
+            match token with
+            | "<"  -> (<)
+            | ">"  -> (>)
+            | "<=" -> (<=)
+            | ">=" -> (>=)
+            | "==" -> (=)
+            | "!=" -> (<>)
+            | _    -> failwith "Not supported"
+        let values =
+            Regex.Match(line, @"([a-z]+) (inc|dec) (-?\d+) if ([a-z]+) ([<>!=]+) (-?\d+)").Groups
+            |> Seq.cast<Group>
+            |> Seq.map (fun g -> g.Value)
+            |> Seq.toList
+        {
+            registerToModify = values.[1];
+            change = (if values.[2] = "inc" then 1 else -1) * (values.[3] |> int);
+            conditionalRegister = values.[4];
+            predicate = getPredicate values.[5];
+            compareValue = values.[6] |> int;
+        }
+
+    input |> List.map parseLine
+
+let initRegisters instructions =
+    instructions
+    |> List.map (fun i -> i.registerToModify)
+    |> Seq.distinct
+    |> Seq.map (fun register -> (register, 0))
+    |> Map.ofSeq
+
+let getMaxValue registers =
+    registers
+    |> Map.toList
+    |> List.map snd
+    |> List.max
+
 let getResultA (input:string list) =
-    let instructions =
-        let parseLine line =
-            let getPredicate token =
-                match token with
-                | "<"  -> (<)
-                | ">"  -> (>)
-                | "<=" -> (<=)
-                | ">=" -> (>=)
-                | "==" -> (=)
-                | "!=" -> (<>)
-                | _    -> failwith "Not supported"
-            let values =
-                Regex.Match(line, @"([a-z]+) (inc|dec) (-?\d+) if ([a-z]+) ([<>!=]+) (-?\d+)").Groups
-                |> Seq.cast<Group>
-                |> Seq.map (fun g -> g.Value)
-                |> Seq.toList
-            {
-                registerToModify = values.[1];
-                change = (if values.[2] = "inc" then 1 else -1) * (values.[3] |> int);
-                conditionalRegister = values.[4];
-                predicate = getPredicate values.[5];
-                compareValue = values.[6] |> int;
-            }
-
-        input |> List.map parseLine
-
-    let registers =
-        instructions
-        |> List.map (fun i -> i.registerToModify)
-        |> Seq.distinct
-        |> Seq.map (fun register -> (register, 0))
-        |> Map.ofSeq
+    let instructions = getInstructions input
+    let registers = initRegisters instructions
 
     let folder (state:Map<string, int>) instruction =
         match instruction.predicate state.[instruction.conditionalRegister] instruction.compareValue with
@@ -52,12 +61,25 @@ let getResultA (input:string list) =
 
     instructions
     |> List.fold folder registers
-    |> Map.toList
-    |> List.map snd
-    |> List.max
+    |> getMaxValue
+
+let getResultB (input:string list) =
+    let instructions = getInstructions input
+    let registers = initRegisters instructions
+
+    let folder ((state:Map<string, int>), maxValue) instruction =
+        match instruction.predicate state.[instruction.conditionalRegister] instruction.compareValue with
+        | true  ->
+            let newState = state |> Map.add instruction.registerToModify (state.[instruction.registerToModify] + instruction.change)
+            newState, max maxValue (newState |> getMaxValue)
+        | false -> state, maxValue
+
+    instructions
+    |> List.fold folder (registers, 0)
+    |> snd
 
 let getResult part (input:string list) =
     match part with
     | A -> getResultA input
-    | B -> failwith "Not implemented yet"
+    | B -> getResultB input
     |> string
