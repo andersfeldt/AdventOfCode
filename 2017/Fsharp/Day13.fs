2a14188be99fdc534d74fdd4971bcffab090660f
Anders Feldt <anders@andersfeldt.se>
2017-12-16 20:22:47 +0100

    Day 13 B

--- 2017/Fsharp/Day13.fs
+++ 2017/Fsharp/Day13.fs
@@ -2,22 +2,34 @@ module Day13
 
 open System
 
+let parse (line:string) =
+    line.Split(": ".ToCharArray(), StringSplitOptions.RemoveEmptyEntries)
+    |> Array.map int
+    |> (fun x -> (x.[0], x.[1]))
+
+let isAtTop (time, range) =
+    (time % (2 * range - 2)) = 0
+
 let getResultA (input:string list) =
-    let parse (line:string) =
-        line.Split(": ".ToCharArray(), StringSplitOptions.RemoveEmptyEntries)
-        |> Array.map int
-        |> (fun x -> (x.[0], x.[1]))
-
-    let isAtTop (time, range) =
-        (time % (2 * range - 2)) = 0
-
     input
     |> List.map parse
     |> List.filter isAtTop
     |> List.sumBy (fun (depth, range) -> depth * range)
 
+let getResultB (input:string list) =
+    let canPass delay =
+        let severity =
+            input
+            |> List.map (parse >> (fun (depth, range) -> (depth + delay, range)))
+            |> List.filter isAtTop
+            |> List.sumBy (fun (depth, range) -> depth * range)
+        severity = 0
+
+    Seq.initInfinite id
+    |> Seq.findIndex canPass
+
 let getResult part (input:string list) =
     match part with
     | A -> getResultA input
-    | B -> failwith "Not implemented yet"
+    | B -> getResultB input
     |> string
