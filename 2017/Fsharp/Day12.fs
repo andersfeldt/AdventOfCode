a31b84f42e256eedbfd4b40ead6669cf89c0b87a
Anders Feldt <anders@andersfeldt.se>
2017-12-19 22:03:13 +0100

    Day 12 B

--- 2017/Fsharp/Day12.fs
+++ 2017/Fsharp/Day12.fs
@@ -2,14 +2,12 @@ module Day12
 
 open System
 
-let getResultA (input:string list) =
-    let parse (line:string) =
-        line.Split("<-> ,".ToCharArray(), StringSplitOptions.RemoveEmptyEntries)
-        |> Array.map int
-        |> Set.ofArray
+let parse (line:string) =
+    line.Split("<-> ,".ToCharArray(), StringSplitOptions.RemoveEmptyEntries)
+    |> Array.map int
+    |> Set.ofArray
 
-    let data = input |> List.map parse
-
+let getGroup data initialSet =
     let result initialState =
         let folder state current =
             let isIntersecting =
@@ -30,12 +28,44 @@ let getResultA (input:string list) =
         (Set.count selectedSet) = lastLength
 
     Seq.initInfinite id
-    |> Seq.scan scanner ((Set.ofList [0]), 0)
+    |> Seq.scan scanner (initialSet, 0)
     |> Seq.find stopCondition
+
+let getResultA (input:string list) =
+    let data = List.map parse input
+
+    getGroup data (Set.ofList [0])
     |> snd
+
+let getResultB (input:string list) =
+    let scanner ((state:Set<int> list), (minElement:int), (count:int)) _ =
+        let group, _ = getGroup state (Set.ofList [minElement])
+
+        let newState =
+            state
+            |> List.filter (fun set -> Set.intersect group set |> Set.isEmpty)
+
+        let newMinElement =
+            match newState with
+            | [] -> 0
+            | newState' ->
+                (newState'
+                |> List.map (Set.minElement)
+                |> List.min)
+
+        (newState, newMinElement, count + 1)
+
+    let stopCondition (state, _, _) = List.isEmpty state
+
+    let initialState = List.map parse input
+
+    Seq.initInfinite id
+    |> Seq.scan scanner (initialState, 0, 0)
+    |> Seq.find stopCondition
+    |> fun (_, _, i) -> i
 
 let getResult part (input:string list) =
     match part with
     | A -> getResultA input
-    | B -> failwith "Not implemented yet"
+    | B -> getResultB input
     |> string
