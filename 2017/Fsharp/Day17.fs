ee3d57b6baeaae2757e694ef1eafcc89d10ccd80
Anders Feldt <anders@andersfeldt.se>
2017-12-20 19:53:05 +0100

    Day 17 B

--- 2017/Fsharp/Day17.fs
+++ 2017/Fsharp/Day17.fs
@@ -19,8 +19,50 @@ let getResultA (input:string) =
 
     buffer.[position + 1]
 
+type State =
+    {
+        length: int;
+        currentPosition: int;
+        zeroPosition: int;
+        valueAfterZero: int;
+    }
+
+let getResultB (input:string) =
+    let initialState =
+        {
+            length = 2;
+            currentPosition = 1;
+            zeroPosition = 0;
+            valueAfterZero = 1;
+        }
+
+    let stepSize = input |> int
+
+    let folder state value =
+        let insertPosition = ((state.currentPosition + stepSize) % (state.length)) + 1
+        let newZeroPosition, newValueAfterZero =
+            match insertPosition with
+            | i when i <= state.zeroPosition    -> state.zeroPosition + 1, state.valueAfterZero
+            | i when i = state.zeroPosition + 1 -> state.zeroPosition, value
+            | _                                 -> state.zeroPosition, state.valueAfterZero
+
+        {
+            length = state.length + 1;
+            currentPosition = insertPosition;
+            zeroPosition = newZeroPosition;
+            valueAfterZero = newValueAfterZero;
+        }
+
+    let stopCondition state = state.length > 50000000
+
+    Seq.initInfinite id
+    |> Seq.skip 2
+    |> Seq.scan folder initialState
+    |> Seq.find stopCondition
+    |> (fun s -> s.valueAfterZero)
+
 let getResult part (input:string list) =
     match part with
     | A -> getResultA input.[0]
-    | B -> failwith "Not implemented yet"
+    | B -> getResultB input.[0]
     |> string
